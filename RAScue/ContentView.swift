7d6cb1c4a931e16c513fefcc52df202550f4e5c1
noob3er <noob3er@gmail.com>
2024-07-22 14:07:00 +0900

    Initial Commit

---
+++ RAScue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RAScue
+//
+//  Created by 준기 on 7/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RAScue/RAScueApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAScueApp.swift
+//  RAScue
+//
+//  Created by 준기 on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct RAScueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
